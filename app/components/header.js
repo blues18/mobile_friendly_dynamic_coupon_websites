b3aeeafce5f1e39efb43e6e26441f5f798ea380e
Blue(derrick) <derricklim13@gmail.com>
2023-06-06 10:52:57 +0800

    Our First Commit Master
    6/6/2023 MP Project

---
+++ app/components/header.js
@@ -0,0 +1,10 @@
+
+export default function Header() {
+  return (
+    <>
+      <nav className="flex flex-col items-center justify-center w-full h-24 border-t">
+        <h1 className="text-danger">Testing header</h1>
+      </nav>
+    </>
+  );
+}
---
+++ app/components/navbar.js
@@ -0,0 +1,46 @@
+"use client";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+
+//import "../styles/navbar.css";
+
+export default function NavigationBar() {
+  return (
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand href="#home">Coupon Website</Navbar.Brand> // This is the
+        brand name
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Nav>
+            {" "}
+            //right nav item
+            <Nav.Link href="Home">Home</Nav.Link>
+            <Nav.Link href="#Shop_Coupon">Shop Coupon</Nav.Link>
+            <Nav.Link eventKey={2} href="About">
+              About
+            </Nav.Link>
+            <Nav.Link className="bi bi-person-fill-gear">Profile</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
